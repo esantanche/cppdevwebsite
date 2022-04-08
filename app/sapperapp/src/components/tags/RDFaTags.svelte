ec1f7ed818936d357bfc12fa7c731fd66fc79861
Emanuele Santanche <esantanche@gmail.com>
2022-04-08 16:13:29 +0200

    Added Google Tag and RDF data

---
+++ app/sapperapp/src/components/tags/RDFaTags.svelte
@@ -0,0 +1,63 @@
+<!--
+@file FIXME
+-->
+
+<section vocab="http://schema.org/" typeof="Service">
+    <meta property="serviceType" content="C++ Software Development"/>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="United Kingdom"/>
+    </span>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="United States of America"/>
+    </span>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="Sweden"/>
+    </span>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="Belgium"/>
+    </span>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="Finland"/>
+    </span>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="Denmark"/>
+    </span>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="Norway"/>
+    </span>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="Germany"/>
+    </span>
+    <span property="areaServed" typeof="Country">
+        <meta property="name" content="France"/>
+    </span>
+
+    <section property="provider" typeof="Person">
+
+        <section property="address" typeof="PostalAddress">
+
+            <meta property="addressLocality" content="Paris"/>
+
+            <section property="addressCountry" typeof="Country">
+                <meta property="name" content="France"/>
+            </section>
+
+        </section>
+
+        <meta property="email" content="emanuele@emanuelesantanche.com"/>
+
+        <section property="address" typeof="PostalAddress">
+
+            <meta property="addressLocality" content="Paris"/>
+
+            <section property="addressCountry" typeof="Country">
+                <meta property="name" content="France"/>
+            </section>
+
+        </section>
+
+        <meta property="telephone" content="+33644755879"/>
+
+        <meta property="name" content="Emanuele Santanche"/>
+    </section>
+</section>
--- app/sapperapp/src/routes/_layout.svelte
+++ app/sapperapp/src/routes/_layout.svelte
@@ -3,6 +3,7 @@
 	import {APP_CONFIGURATION} from '../appConfiguration';
 	import StandardFooter from '../components/footers/StandardFooter.svelte';
 	import StandardHeader from '../components/headers/StandardHeader.svelte';
+	import RDFaTags from "../components/tags/RDFaTags.svelte";
 
 	export const segment: string = "";
 	
@@ -17,6 +18,20 @@
 		box-sizing: border-box;
 	}
 </style>
+
+<svelte:head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-C1JNY5EBLF"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-C1JNY5EBLF');
+	</script>
+</svelte:head>
+
+<RDFaTags />
 
 <main style="--max-width: {APP_CONFIGURATION.maxwidthforcontent};"  >
 	<StandardHeader />
